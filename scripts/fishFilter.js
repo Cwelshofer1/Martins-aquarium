5419f75398d45f80ba905308edbe94f2b0fe6d21
Cwelshofer1 <sweetbrooo@gmail.com>
2025-01-20 19:18:35 -0600

    finished chapter 5 work

--- scripts/fishFilter.js
+++ scripts/fishFilter.js
@@ -1,18 +1,5 @@
 import { database } from './aquariumData.js';
 
-
-// const numbers = [4,1,5,8,6,4,2,2,4,6,7,7,8,9,7,5,3,2,1,3,4,2,1,3,5,7,76,5,9,4,23,5,3,2,6,8,98,65,3,1,3,4]
-
-// // Only display even numbers
-// for (const number of numbers) {
-//     if (number % 2 === 0) {
-//         console.log(number)
-//     }
-// }
-
-
-
-
 export const mostHolyFish = () => {
     // 3, 6, 9, 12, etc... fish
     const holyFish = []
@@ -25,6 +12,47 @@ export const mostHolyFish = () => {
     return holyFish
    
 }
+
+export const holyFishList = () => {
+
+    const fishListTwo = mostHolyFish()
+    let holyFishHTML = ""
+    
+
+    for (const fish of fishListTwo) {
+         holyFishHTML += `
+            <article class="fish">
+             <h2 class="fish__name">${fish.name}</h2>
+                <p class="fish__species">${fish.species}</p>
+                <p class="fish__diet"> Diet:${fish.diet}</p>
+                <p class="fish__diet"> Length:${fish.length}</p>
+                <p class="fish__diet"> Location:${fish.location}</p>
+                <img src="${fish.image}" alt="${fish.species}" class="fish_image">
+                <div class="fish__details">
+                   
+                </div>
+            </article>
+        `;
+    }
+    
+    
+
+    return holyFishHTML
+    
+    // Generate an HTML representation of each fish
+};
+
+export const renderHolyFishToDOM = (holyFishHTML) => {
+    let holyFishesList = document.getElementById('holylist');
+ 
+    if (holyFishesList) {
+        holyFishesList.innerHTML = holyFishHTML;
+    } else {
+        console.error('Could not find element with id "fish-list"');
+    }
+ };
+
+
 console.log(mostHolyFish())
 
 export const soldierFish = () => {
@@ -39,10 +67,50 @@ export const soldierFish = () => {
      return soldierFishes
      
 }
+
+export const soldierFishList = () => {
+
+    const fishListOne = soldierFish()
+    let soldierFishHTML = ""
+    
+
+    for (const fish of fishListOne) {
+         soldierFishHTML += `
+            <article class="fish">
+             <h2 class="fish__name">${fish.name}</h2>
+                <p class="fish__species">${fish.species}</p>
+                <p class="fish__diet"> Diet:${fish.diet}</p>
+                <p class="fish__diet"> Length:${fish.length}</p>
+                <p class="fish__diet"> Location:${fish.location}</p>
+                <img src="${fish.image}" alt="${fish.species}" class="fish_image">
+                <div class="fish__details">
+                   
+                </div>
+            </article>
+        `;
+    }
+    
+    
+
+    return soldierFishHTML
+    
+    // Generate an HTML representation of each fish
+};
+
+export const renderSoldierFishToDOM = (soldierFishHTML) => {
+    let soldierFishesList = document.getElementById('soldierlist');
+ 
+    if (soldierFishesList) {
+        soldierFishesList.innerHTML = soldierFishHTML;
+    } else {
+        console.error('Could not find element with id "fish-list"');
+    }
+ };
+
 console.log(soldierFish())
 
 export const regularFish = () => {
-     // 3, 6, 9, 12, etc... fish
+     
      const regularFishes = []
      for (const fishes of database.fish) {
          if (fishes.length % 5 !== 0 && fishes.length % 3 !== 0){
@@ -54,3 +122,42 @@ export const regularFish = () => {
      
 }
 console.log(regularFish())
+
+export const regularFishList = () => {
+
+    const fishList = regularFish()
+    let regularFishHTML= ""
+    
+
+    for (const fish of fishList) {
+         regularFishHTML += `
+            <article class="fish">
+             <h2 class="fish__name">${fish.name}</h2>
+                <p class="fish__species">${fish.species}</p>
+                <p class="fish__diet"> Diet:${fish.diet}</p>
+                <p class="fish__diet"> Length:${fish.length}</p>
+                <p class="fish__diet"> Location:${fish.location}</p>
+                <img src="${fish.image}" alt="${fish.species}" class="fish_image">
+                <div class="fish__details">
+                   
+                </div>
+            </article>
+        `;
+    }
+    
+    
+
+    return regularFishHTML
+    
+   
+};
+
+export const renderRegularFishToDOM = (regularFishHTML) => {
+    let regularFishesList = document.getElementById('regularlist');
+ 
+    if (regularFishesList) {
+        regularFishesList.innerHTML = regularFishHTML;
+    } else {
+        console.error('Could not find element with id "fish-list"');
+    }
+ };
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,19 @@
-import { fishList } from './fishList.js'
-import { renderFishToDOM } from './fishList.js'
+// import { fishList } from './fishList.js'
+// import { renderFishToDOM } from './fishList.js'
 import { tipList } from './tipList.js'
 import { renderTipsToDOM } from './tipList.js'
 import { locationList } from './locationList.js'
 import { renderLocationsToDOM } from './locationList.js'
-
+import { renderHolyFishToDOM, holyFishList } from './fishFilter.js'
+import { renderSoldierFishToDOM, soldierFishList } from './fishFilter.js'
+import { renderRegularFishToDOM, regularFishList } from './fishFilter.js'
+// import { soldierFish } from './fishFilter.js'
+// import { regularFish } from './fishFilter.js'
 
 
 // Generate the fish list
-const fishHTML = fishList() 
-renderFishToDOM(fishHTML)
+// const fishHTML = fishList() 
+// renderFishToDOM(fishHTML)
 
 // // Generate the care tips
 const tipHTML = tipList()
@@ -20,5 +24,11 @@ const locationHTML = locationList()
 renderLocationsToDOM(locationHTML)
 
 // Render each HTML string to the correct DOM element
+const holyFishHTMLTwo = holyFishList()
+renderHolyFishToDOM(holyFishHTMLTwo)
 
+const soldierFishHTML = soldierFishList()
+renderSoldierFishToDOM(soldierFishHTML)
 
+const regularFishHTML = regularFishList()
+renderRegularFishToDOM(regularFishHTML)
